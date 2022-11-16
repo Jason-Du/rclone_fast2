b9397b33495c92e599c9007f122838cf41b9ed03
“Jason-Duh(Home) <dushiun@gmail.com>
2022-11-16 19:21:53 +0800

    update

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # -q 安靜，Action 不輸出詳細傳輸資料名和過程。
-rclone --checksum sync google_drive: onedrive: --transfers 48 -q --create-empty-src-dirs --fast-list
+rclone --checksum sync TNSSH_drive:./ dushiun:TNSSH_BACKUP/ --transfers 48 -q --create-empty-src-dirs --fast-list
 
 #rclone --checksum sync "$DRIVE_SOURCE": "$DRIVE_TARGET": --transfers 48 -q --drive-server-side-across-configs --create-empty-src-dirs --fast-list
